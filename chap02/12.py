92e94063765a0e4d8b0a25c1ac3f066ffec5b70c
mkan0141 <kanzume0141@gmail.com>
2018-06-11 10:49:05 +0900

    fix: 答えが間違っていたため修正

--- chap02/12.py
+++ chap02/12.py
@@ -3,5 +3,5 @@ col1 = open('col1.txt', 'w')
 col2 = open('col2.txt', 'w')
 
 for x in data.readlines():
-    col1.write(x[0])
-    col2.write(x[1])
+    col1.write(x.split()[0] + '\n')
+    col2.write(x.split()[1] + '\n')
