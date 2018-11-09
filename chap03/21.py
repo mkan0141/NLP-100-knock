740a86ab0341f0f3013baa744162dacef0ea75db
mkan0141 <kanzume0141@gmail.com>
2018-11-09 16:33:51 +0900

    fix

--- chap03/21.py
+++ chap03/21.py
@@ -1,10 +1,15 @@
 import json
+import re
 
-data = open('jawiki-country.json', 'r')
-text = data.readlines()
+def select_title(word):
+    data =open('jawiki-country.json', 'r').readlines()
+    return [json.loads(line)['text'] for line in data if json.loads(line)['title'] == word]
 
-data = [json.loads(a) for a in text if json.loads(a)['title'] == 'イギリス']
 
+data = select_title('イギリス')
+regix = '.*Category.*'
+pattern = re.compile(regix)
+
+data = pattern.findall(''.join(data))
 for d in data:
-    if 'Category' in d['text']:
-        print(d['text'])
+    print(d)
